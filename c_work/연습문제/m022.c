469f468a44a7381ec7fcebdf7cc92fcd54b7d2c7
gitcat87 <qkdlvj12@naver.com>
2022-12-08 10:58:26 +0900

    221208

---
+++ c_work/연습문제/m022.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("최대현:\n");
+    printf("01084953325\n");
+    printf("대구시 달서구");
+
+    return 0;
+}
---
+++ c_work/연습문제/m024.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+int main(){
+    
+    int a = "4";
+    int b = "5";
+    int c = "7";
+    int d = "9";
+
+    printf("a*b=")
+
+    return 0;
+    
+}
